5379bd0c86b6f98c7d0115ba0e4be2b297a0823f
RAJNI SAINI <154338504+Rajni218@users.noreply.github.com>
2026-04-17 16:36:06 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,18 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    if (!process.env.MONGODB_URI) {
+      console.error("MONGODB_URI is not defined");
+      process.exit(1);
+    }
+
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
+      dbName: "hrsystem", 
     });
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`Database Name: ${conn.connection.name}`);
+
   } catch (error) {
     console.error(`MongoDB connection error: ${error.message}`);
     process.exit(1);
